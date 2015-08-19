c802fd66931d85dc299247d62bd6a788fad87b44
Victor Perin <me@victorperin.com>
2015-08-19 02:02:33 -0300

    Inserindo tratamento de erro

--- convert-code-to-char.js
+++ convert-code-to-char.js
@@ -1,5 +1,8 @@
 function convertCodeToChar(code){
-
+  if(code>=64){
+    console.log("Erro! Não coloque um número maior ou igual a 64");
+    process.exit(1);
+  }
   var digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
   var char = digitos.charAt(code);
   return char;
