6a52bbf233a9507d43758327bc9d20bff222314c
Victor Perin <me@victorperin.com>
2015-08-19 01:44:08 -0300

    Corrigindo código pois a tabela ascii tem alguns caracteres antes das letras... :(

--- testes/getchar.js
+++ testes/getchar.js
@@ -1,4 +1,6 @@
 // para executar com o nodejs apenas execute no terminal 'node getchar.js 10' ou qualquer outro número para ver o que ele retorna
-var number = parseInt(process.argv[2]) + 48;
-var chr = String.fromCharCode(number);
+
+var number = parseInt(process.argv[2]);
+var digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+var chr = digitos.charAt(number);
 console.log(chr);
