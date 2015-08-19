e5415cf784b81d691d455f85bb7db863f640f2f3
Victor Perin <me@victorperin.com>
2015-08-19 01:57:58 -0300

    Inserindo tratamento de erro

--- convert-char-to-code.js
+++ convert-char-to-code.js
@@ -1,6 +1,9 @@
 function convertCharToCode(char){
   var digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
   var numero = digitos.indexOf(char);
-
+  if(numero<0){
+    console.log("Erro! Não coloque uma string fora da cadeia de characteres");
+    process.exit(1);
+  }
   return numero;
 }
