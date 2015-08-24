43d43818218120f62635de5a97013d5f9d32e31c
Victor Perin <me@victorperin.com>
2015-08-24 17:17:07 -0300

    inserindo parte fracionaria como variavel do resultado

--- decimal-to-other.js
+++ decimal-to-other.js
@@ -3,7 +3,10 @@ var convertCharToCode = require('./convert-code-to-char.js');
 function decimalToOther(numero,base){
 
   var resultInteiro = "";
+  var restultFracionario = "";
+
   var inteiro = Math.floor(numero);
+  var fracionario = Number("."+(numero+"").split(".")[1]);
 
   while (inteiro != 0) {
     var q = inteiro % base;
@@ -11,7 +14,7 @@ function decimalToOther(numero,base){
     inteiro = (inteiro - q) / base;
   }
 
-  var result = resultInteiro;
+  var result = resultInteiro + ((fracionario)?"." + restultFracionario:"");
 
   return result;
 }
