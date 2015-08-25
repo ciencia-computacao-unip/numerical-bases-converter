9d858e07b92c5f7ab4ab6f1d6df419bd626ac587
Victor Perin <me@victorperin.com>
2015-08-25 15:00:07 -0300

    Compartilhando variável dos digitos

--- convert-code-to-char.js
+++ convert-code-to-char.js
@@ -1,9 +1,10 @@
+var digitos = require('./digitos.js');
+
 module.exports = function (code){
   if(code>=64){
     console.log("Erro! Não coloque um número maior ou igual a 64");
     process.exit(1);
   }
-  var digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
   var char = digitos.charAt(code);
   return char;
 }
--- convertCharToCode.js
+++ convertCharToCode.js
@@ -1,5 +1,6 @@
+var digitos = require('./digitos.js');
+
 module.exports = function (char){
-  var digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
   var numero = digitos.indexOf(char);
   if(numero<0){
     console.log("Erro! Não coloque uma string fora da cadeia de characteres");
